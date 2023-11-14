3c4ae5e4b6459ace13d1147fa058524f9d7ae3e7
Gregorio <Greg.del801@gmail.com>
2023-11-14 10:46:13 -0700

    completed code-along

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
+import ipdb
 
 def happy_new_year():
     # code goes here!
-    pass
+    for i in range(11):
+        print(i)
+        while i >= 1:
+            i -= 1
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_ints = [num * num for num in int_list]
+    return squared_ints
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
